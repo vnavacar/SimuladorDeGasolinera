6d043fbeccd956a3c06990889664ee62adad2236
vnavacar <98817213+vnavacar@users.noreply.github.com>
2022-10-25 10:01:19 +0200

    asdf

--- src/App.java
+++ src/App.java
@@ -1,4 +1,10 @@
 public class App {
+
+    //N surtidores
+    //una sola caja para pagar
+    //utilizar executor
+
+    //luego swingworker
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
